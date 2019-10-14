f6a5fb8720430b0b77253f915147bb23ffe48dd3
HankyZ <hankyzhang@gmail.com>
2019-10-14 19:15:04 -0400

    updated solution for problem 4

--- 004. Median of Two Sorted Arrays/Solution.java
+++ 004. Median of Two Sorted Arrays/Solution.java
@@ -2,11 +2,15 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int size = nums1.length + nums2.length;
 
+        if (size == 1)
+            return nums2.length == 0 ? nums1[0] : nums2[0];
+
         int counter = 0;
         int index1 = 0;
         int index2 = 0;
+        int medians[] = {0, 0};
 
-        while ((size % 2 == 0 && counter < size / 2 - 1) || (size % 2 == 1 && counter < size / 2)) {
+        while (counter < size / 2 - 1) {
 
             if (index1 >= nums1.length)
                 index2++;
@@ -20,45 +24,25 @@ class Solution {
             counter++;
         }
 
-        if (size % 2 == 0) {
+        for (int i = 0; i < medians.length; i++) {
 
             if (index1 >= nums1.length)
-                return ((double) nums2[index2] + (double) nums2[++index2]) / 2;
+                medians[i] = nums2[index2++];
 
-            if (index2 >= nums2.length)
-                return ((double) nums1[index1] + (double) nums1[++index1]) / 2;
+            else if (index2 >= nums2.length)
+                medians[i] = nums1[index1++];
 
-            int medians[] = new int[2];
+            else if (nums1[index1] <= nums2[index2])
+                medians[i] = nums1[index1++];
 
-            for (int i = 0; i < medians.length; i++) {
-
-                if (index1 >= nums1.length) {
-                    medians[i] = nums2[index2];
-                    index2++;
-                } else if (index2 >= nums2.length) {
-                    medians[i] = nums1[index1];
-                    index1++;
-                } else if (nums1[index1] <= nums2[index2]) {
-                    medians[i] = nums1[index1];
-                    index1++;
-                } else {
-                    medians[i] = nums2[index2];
-                    index2++;
-                }
-            }
-            return ((double) medians[0] + (double) medians[1]) / 2;
+            else
+                medians[i] = nums2[index2++];
         }
 
-        if (index1 >= nums1.length)
-            return nums2[index2];
+        if (size % 2 == 1)
+            return medians[1];
 
-        if (index2 >= nums2.length)
-            return nums1[index1];
-
-        if (nums1[index1] <= nums2[index2])
-            return nums1[index1];
-
-        return nums2[index2];
+        return ((double) medians[0] + (double) medians[1]) / 2;
     }
 
 }
