91d0dc77a84cf3dc005136a5c633541c3d32c2f7
HankyZ <hankyzhang@gmail.com>
2019-10-23 14:23:16 -0400

    create problem 222

---
+++ 222. Count Complete Tree Nodes GG/Solution.java
@@ -0,0 +1,16 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public int countNodes(TreeNode root) {
+
+        return 0;
+    }
+}
