c10334328aced8510cd0f215544ee9c8b5b927a9
HankyZ <hankyzhang@gmail.com>
2019-10-23 21:22:34 -0400

    added solution to problem 222

--- 222. Count Complete Tree Nodes GG/Solution.java
+++ 222. Count Complete Tree Nodes GG/Solution.java
@@ -1,3 +1,5 @@
+import static java.lang.Math.pow;
+
 class TreeNode {
     int val;
     TreeNode left;
@@ -9,8 +11,59 @@ class TreeNode {
 }
 
 class Solution {
+
+    private int countDepth(TreeNode node) {
+        int depth = 0;
+        while (node != null) {
+            node = node.right;
+            depth++;
+        }
+        return depth;
+    }
+
+    private boolean exists(TreeNode node, int depth, int position) {
+
+        int lp = 0, rp = (int) pow(2, depth) - 1;
+
+        for (int i = 0; i < depth; i++) {
+            if (position > lp + (rp - lp) / 2) {
+                node = node.right;
+                lp = lp + (rp - lp) / 2 + 1;
+            } else {
+                node = node.left;
+                rp = lp + (rp - lp) / 2;
+            }
+        }
+        return node != null;
+    }
+
     public int countNodes(TreeNode root) {
 
-        return 0;
+        if (root == null)
+            return 0;
+
+        int depth = countDepth(root);
+
+        int lp = 0;
+        int rp = (int) pow(2, depth) - 1;
+
+        while (lp + 1 < rp) {
+
+            if (exists(root, depth, lp + (rp - lp) / 2 + 1)) {
+                lp = lp + (rp - lp) / 2 + 1;
+            } else {
+                rp = lp + (rp - lp) / 2;
+            }
+        }
+
+        int numExtraNodes = lp;
+
+        if (exists(root, depth, lp))
+            numExtraNodes++;
+
+        if (exists(root, depth, rp))
+            numExtraNodes++;
+
+        return (int) pow(2, depth) - 1 + numExtraNodes;
     }
 }
