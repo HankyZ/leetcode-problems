3376de1240af588b89aef26b75a98dfba5414e54
HankyZ <hankyzhang@gmail.com>
2019-11-27 15:54:46 -0500

    added solution to problem 410

---
+++ 410. Split Array Largest Sum GG/Solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int splitArray(int[] nums, int m) {
+        int n = nums.length;
+        int[][] f = new int[n + 1][m + 1];
+        int[] sum = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                f[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+            sum[i + 1] = sum[i] + nums[i];
+
+        f[0][0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                for (int k = 0; k < i; k++) {
+                    f[i][j] = Math.min(f[i][j], Math.max(f[k][j - 1], sum[i] - sum[k]));
+                }
+            }
+        }
+
+        return f[n][m];
+    }
+}
